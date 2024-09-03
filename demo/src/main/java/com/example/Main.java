d6aede89ee089857c2fa182e9220ef6d4a1b686d
Matias Davezac <8675@holbertonstudents.com>
2024-09-03 13:46:16 -0300

    Doing stuff

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+// java -cp target/demo-1.0-SNAPSHOT.jar com.example.Main
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
